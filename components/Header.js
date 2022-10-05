274520badb7a6fdfe5e548726f99afa48bc14747
rumgal <rahimhamadou@gmail.com>
2022-10-05 17:03:52 +0200

    news-part 5 upd frontend

--- components/Header.js
+++ components/Header.js
@@ -26,7 +26,7 @@ function Header() {
 	}, []);
 
 	const handleRegister = () => {
-		fetch("http://localhost:3000/users/signup", {
+		fetch("https://news-app-backend-green.vercel.app/users/signup", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
@@ -43,7 +43,7 @@ function Header() {
 	};
 
 	const handleConnection = () => {
-		fetch("http://localhost:3000/users/signin", {
+		fetch("https://news-app-backend-green.vercel.app/users/signin", {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({ username: signInUsername, password: signInPassword }),
--- components/TopArticle.js
+++ components/TopArticle.js
@@ -1,12 +1,10 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { addBookmark, removeBookmark } from '../reducers/bookmarks';
-import styles from '../styles/TopArticle.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookmark } from '@fortawesome/free-solid-svg-icons';
-
+import { useDispatch, useSelector } from "react-redux";
+import { addBookmark, removeBookmark } from "../reducers/bookmarks";
+import styles from "../styles/TopArticle.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 function TopArticle(props) {
-
 	const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
 
@@ -15,9 +13,9 @@ function TopArticle(props) {
 			return;
 		}
 
-		fetch(`http://localhost:3000/users/canBookmark/${user.token}`)
-			.then(response => response.json())
-			.then(data => {
+		fetch(`https://news-app-backend-green.vercel.app/users/canBookmark/${user.token}`)
+			.then((response) => response.json())
+			.then((data) => {
 				if (data.result && data.canBookmark) {
 					if (props.isBookmarked) {
 						dispatch(removeBookmark(props));
@@ -26,11 +24,11 @@ function TopArticle(props) {
 					}
 				}
 			});
-	}
+	};
 
 	let iconStyle = {};
 	if (props.isBookmarked) {
-		iconStyle = { 'color': '#E9BE59' };
+		iconStyle = { color: "#E9BE59" };
 	}
 
 	return (
@@ -38,7 +36,12 @@ function TopArticle(props) {
 			<img src={props.urlToImage} className={styles.image} alt={props.title} />
 			<div className={styles.topText}>
 				<h2 className={styles.topTitle}>{props.title}</h2>
-				<FontAwesomeIcon onClick={() => handleBookmarkClick()} icon={faBookmark} style={iconStyle} className={styles.bookmarkIcon} />
+				<FontAwesomeIcon
+					onClick={() => handleBookmarkClick()}
+					icon={faBookmark}
+					style={iconStyle}
+					className={styles.bookmarkIcon}
+				/>
 				<h4>{props.author}</h4>
 				<p>{props.description}</p>
 			</div>
